2e260ccb8200984d9616b500a861d0d005bbb863
devlitus <developercarles@gmail.com>
2025-04-20 10:40:18 +0200

    feat(constants): agregar clase para definir constantes de espaciado
    Se añade la clase `Spacing` que define constantes de espaciado para
    mantener la consistencia en la interfaz de usuario. Esto facilitará
    el uso de espaciados uniformes en toda la aplicación.

---
+++ lib/core/constants/spacing.dart
@@ -0,0 +1,20 @@
+/// Define las constantes de espaciado para mantener consistencia en la UI
+class Spacing {
+  /// Espacio extra pequeño - 4.0
+  static const double xs = 4.0;
+
+  /// Espacio pequeño - 8.0
+  static const double s = 8.0;
+
+  /// Espacio mediano - 16.0
+  static const double m = 16.0;
+
+  /// Espacio grande - 24.0
+  static const double l = 24.0;
+
+  /// Espacio extra grande - 32.0
+  static const double xl = 32.0;
+
+  /// Espacio doble extra grande - 48.0
+  static const double xxl = 48.0;
+}
